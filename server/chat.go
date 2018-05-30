213d22a8d8f6761a9073e773c34de21451da1378
code0wl <o.lodriguez@gmail.com>
2018-05-30 08:04:59 +0200

    updated client

--- server/chat.go
+++ server/chat.go
@@ -6,7 +6,10 @@ import (
 
 // Room distribute channel that holds incoming messages
 type Room struct {
-	distribute chan []byte
+	forward chan []byte // updates other channels
+	join    chan *Client
+	leave   chan *Client
+	clients map[*Client]bool
 }
 
 // Client represents a user
@@ -14,4 +17,24 @@ type Client struct {
 	socket *websocket.Conn
 	send   chan []byte
 	room   *Room
+}
+
+func (c *Client) read() {
+	for {
+		if _, msg, err := c.socket.ReadMessage(); err == nil {
+			c.room.forward <- msg
+		} else {
+			break
+		}
+	}
+	c.socket.Close()
+}
+
+func (c *Client) write() {
+	for msg := range c.send {
+		if err := c.socket.WriteMessage(websocket.TextMessage, msg); err != nil {
+			break
+		}
+	}
+	c.socket.Close()
 }
